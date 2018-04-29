9f25b70585cdad81a029d5858667c0960765c2c7
Eva Maria Veitmaa <evveit@ttu.ee>
2018-04-29 19:18:50 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+var vm = new Vue({
+    el: "#app",
+    data: function () {
+        return {
+            studentCodeOne: "",
+            studentCodeTwo: "",
+            dijkstraName: "",
+            mainPoints: {
+                waitingLinePoints: 0,
+                sortingPoints: 0,
+                extraTaskPoints: 0,
+                livesPoints: 0,
+                restartPoints: 0,
+                animationPoints: 0
+            },
+            extraPoints: {
+                goodDesign: false,
+                designSupportsTheme: false,
+                goodAppearances: false,
+                goodSortingFeedback: false,
+                goodExtraTaskEpisode: false,
+                goodFailureFeedback: false,
+                feedbackSound: false,
+                learnability: false,
+                dragAndDrop: false,
+                responsiveDesign: false
+            }
+        }
+    },
+    computed: {
+        mainPointsTotal: function () {
+            return parseInt(this.mainPoints.waitingLinePoints)
+                + parseInt(this.mainPoints.sortingPoints)
+                + parseInt(this.mainPoints.extraTaskPoints)
+                + parseInt(this.mainPoints.livesPoints)
+                + parseInt(this.mainPoints.restartPoints)
+                + parseInt(this.mainPoints.animationPoints)
+        },
+        extraPointsTotal: function () {
+            var total = 0;
+            for (item in this.extraPoints) {
+                if (this.extraPoints[item] === true) {
+                    total++;
+                }
+            }
+            return total;
+        }
+    },
+    methods: {}
+});
