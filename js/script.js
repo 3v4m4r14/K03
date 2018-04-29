5e8072eb3a6372f36ef5568fe0e6348f9a1830cd
Eva Maria Veitmaa <evveit@ttu.ee>
2018-04-29 19:52:11 +0300

    Summary

--- js/script.js
+++ js/script.js
@@ -6,13 +6,15 @@ var vm = new Vue({
             studentCodeTwo: "",
             dijkstraName: "",
             submissionSelection: "onTime",
+            penalty: 0,
             mainPoints: {
                 waitingLinePoints: 0,
                 sortingPoints: 0,
                 extraTaskPoints: 0,
                 livesPoints: 0,
                 restartPoints: 0,
-                animationPoints: 0
+                animationPoints: 0,
+                total: 0
             },
             extraPoints: {
                 goodDesign: false,
@@ -24,27 +26,44 @@ var vm = new Vue({
                 feedbackSound: false,
                 learnability: false,
                 dragAndDrop: false,
-                responsiveDesign: false
+                responsiveDesign: false,
+                total: 0
             }
         }
     },
     computed: {
         mainPointsTotal: function () {
-            return parseInt(this.mainPoints.waitingLinePoints)
+            this.mainPoints.total = parseInt(this.mainPoints.waitingLinePoints)
                 + parseInt(this.mainPoints.sortingPoints)
                 + parseInt(this.mainPoints.extraTaskPoints)
                 + parseInt(this.mainPoints.livesPoints)
                 + parseInt(this.mainPoints.restartPoints)
-                + parseInt(this.mainPoints.animationPoints)
+                + parseInt(this.mainPoints.animationPoints);
+            return this.mainPoints.total;
         },
         extraPointsTotal: function () {
-            var total = 0;
+            this.extraPoints.total = 0;
             for (item in this.extraPoints) {
                 if (this.extraPoints[item] === true) {
-                    total++;
+                    this.extraPoints.total++;
                 }
             }
-            return total;
+            return this.extraPoints.total;
+        },
+        latePenaltyTotal: function () {
+            if (this.submissionSelection === "onTime") {
+                this.penalty = 0;
+            }
+            if (this.submissionSelection === "lateOneWeek") {
+                this.penalty = -2;
+            }
+            if (this.submissionSelection === "lateForMore") {
+                this.penalty = -5
+            }
+            return this.penalty;
+        },
+        pointsTotal: function () {
+            return this.mainPoints.total + this.extraPoints.total + this.penalty;
         }
     },
     methods: {}
