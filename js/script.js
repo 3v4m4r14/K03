c5fd6ff202669d882d3d0857ee50785b29aed613
Eva Maria Veitmaa <evveit@ttu.ee>
2018-05-04 11:24:38 +0300

    Kind of a broken solution for recalling past submissions

--- js/script.js
+++ js/script.js
@@ -2,47 +2,81 @@ var vm = new Vue({
     el: "#app",
     data: function () {
         return {
-            deadline: new Date("2015-05-02"), 
+            deadline: new Date("2015-05-02"),
             currentIndex: 0,
             students: [
-                { code: "155408IAPB", name: "Eva Maria Veitmaa" },
-                { code: "155202IAPB", name: "Ingmar Trump" }
+                {code: "155408IAPB", name: "Eva Maria Veitmaa"},
+                {code: "155202IAPB", name: "Ingmar Trump"}
             ],
             works: [
-            {
-                studentCodeOne: "155408IAPB",
-                studentCodeTwo: "155202IAPB",
-                dijkstraAddress: "evveit",
-                taskType: "regular",
-                requirements: {
-                    waitingLine: true,
-                    sorting: true,
-                    extraTask: true,
-                    lives: false,
-                    restarting: true,
-                    animations: true
-                }, 
-                alternativeRequirements: false,
-                requirementsComment: "Elud puudu.",
-                extras: {
-                    goodDesign: false,
-                    designSupportsTheme: false,
-                    goodAppearances: false,
-                    goodSortingFeedback: false,
-                    goodExtraTaskEpisode: false,
-                    goodFailureFeedback: false,
-                    feedbackSound: false,
-                    learnability: false,
-                    dragAndDrop: false,
-                    responsiveDesign: false
-                }, 
-                extraExtraPoints: 0,
-                extrasComment: "", 
-                submitted: "onTime", 
-                submittedComment: "1 nädal pikendust.", 
-                projectPoints: 0, 
-                projectComment: ""
-            }]
+                {
+                    studentCodeOne: "",
+                    studentCodeTwo: "",
+                    dijkstraAddress: "",
+                    taskType: "regular",
+                    requirements: {
+                        waitingLine: false,
+                        sorting: false,
+                        extraTask: false,
+                        lives: false,
+                        restarting: false,
+                        animations: false
+                    },
+                    alternativeRequirements: false,
+                    requirementsComment: "",
+                    extras: {
+                        goodDesign: false,
+                        designSupportsTheme: false,
+                        goodAppearances: false,
+                        goodSortingFeedback: false,
+                        goodExtraTaskEpisode: false,
+                        goodFailureFeedback: false,
+                        feedbackSound: false,
+                        learnability: false,
+                        dragAndDrop: false,
+                        responsiveDesign: false
+                    },
+                    extraExtraPoints: 0,
+                    extrasComment: "",
+                    submitted: "onTime",
+                    submittedComment: "",
+                    projectPoints: 0,
+                    projectComment: ""
+                },
+                {
+                    studentCodeOne: "155408IAPB",
+                    studentCodeTwo: "155202IAPB",
+                    dijkstraAddress: "evveit",
+                    taskType: "regular",
+                    requirements: {
+                        waitingLine: true,
+                        sorting: true,
+                        extraTask: true,
+                        lives: false,
+                        restarting: true,
+                        animations: true
+                    },
+                    alternativeRequirements: false,
+                    requirementsComment: "Elud puudu.",
+                    extras: {
+                        goodDesign: false,
+                        designSupportsTheme: false,
+                        goodAppearances: false,
+                        goodSortingFeedback: false,
+                        goodExtraTaskEpisode: false,
+                        goodFailureFeedback: false,
+                        feedbackSound: false,
+                        learnability: false,
+                        dragAndDrop: false,
+                        responsiveDesign: false
+                    },
+                    extraExtraPoints: 0,
+                    extrasComment: "",
+                    submitted: "onTime",
+                    submittedComment: "1 nädal pikendust.",
+                    projectPoints: 0,
+                    projectComment: ""
+                }]
         }
     },
     computed: {
@@ -101,8 +135,8 @@ var vm = new Vue({
             }
             if (!this.isBaseDone) return 0;
             return 10 + this.extraPointsTotal + this.latePenaltyTotal;
-        }, 
-        address: function() {
+        },
+        address: function () {
             return "http://dijkstra.cs.ttu.ee/~" + this.current.dijkstraAddress + "/ui/t2/";
         },
         taskTypeIsRegular: function () {
@@ -110,11 +144,27 @@ var vm = new Vue({
         },
         taskTypeIsProject: function () {
             return this.current.taskType === "project";
-        }, 
+        },
         getDeadline: function () {
             var months = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
             return this.deadline.getDate() + " " + months[this.deadline.getMonth()] + " " + this.deadline.getFullYear();
         }
     },
-    methods: {}
+    methods: {
+        searchPrevious: function (id) {
+            var studentCode = document.getElementById(id).value;
+            for (var i = 1; i < this.works.length; i++) {
+                var work = this.works[i];
+                if (work.studentCodeOne === studentCode || work.studentCodeTwo === studentCode) {
+                    this.currentIndex = this.getWorkIndex(work);
+                } else {
+                    this.currentIndex = 0;
+                }
+            }
+        },
+        getWorkIndex: function (work) {
+            console.log("I'm here.");
+            return this.works.indexOf(work);
+        }
+    }
 });
